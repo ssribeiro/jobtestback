95077c0d73c06aabcc4a1fc37ccfd6072596e641
SS Ribeiro <luan@ssribeiro.com>
2018-06-08 12:26:43 -0300

    minor fix in updateDays cronjob

--- app/cronjobs/update-days.js
+++ app/cronjobs/update-days.js
@@ -2,6 +2,7 @@ const DaysController = require('../controllers/days');
 
 module.exports = {
   start: ()=>{
+    DaysController.updateDays();
     return setInterval(()=>{ // update days information
       DaysController.updateDays();
     }, 10*60*1000); // each 5 minutes
